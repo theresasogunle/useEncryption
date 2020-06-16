6cdc8927e597bf365fea4753d57cd8bc57899910
Omowunmi <sogunledolapo@gmail.com>
2020-06-16 20:53:17 +0100

    testing custom hook

--- src/useEncryption.js
+++ src/useEncryption.js
@@ -1,4 +1,22 @@
-const useEncryption = ()=>{}
+import CryptoJS from "crypto-js";
+const useEncryption = (key)=>{
+ const encryptData = (data) => {
+  return CryptoJS.AES.encrypt(JSON.stringify(data), key).toString();
+};
+const decryptData = (encryptedData) => {
+  const bytes = CryptoJS.AES.decrypt(encryptedData, key);
+  return bytes.toString(CryptoJS.enc.Utf8);
+};
+return {
+    encryptData,
+    decryptData
+}
+}
 
 
 export default useEncryption
+
+
+
+
+
